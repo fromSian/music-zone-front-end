b88f8e7ef4c064cec257a4e9ad8a3e0d93e2f030
qian <fromSian@163.com>
2024-03-26 20:46:10 +0800

    feat: 頭部導航改為antd Menu實現

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,48 @@
+import { Layout, Menu } from 'antd'
+import { Content } from 'antd/es/layout/layout'
+import type { MenuItemType } from 'antd/es/menu/hooks/useItems'
+import { useEffect, useState } from 'react'
 import type { RouteObject } from 'react-router-dom'
 import { NavLink, Outlet } from 'react-router-dom'
-import styles from './index.module.less'
 
+const { Header } = Layout
 interface HeaderProps {
     routerConfig: (RouteObject & { name: string })[]
 }
 
-const Header = ({ routerConfig }: HeaderProps) => {
-    return (
-        <nav>
+const HeaderCustom = ({ routerConfig }: HeaderProps) => {
+
+
+    const [menus, setMenus] = useState<MenuItemType[]>([])
+
+
+    useEffect(() => {
+
+        const _menus = routerConfig.map(item => (
             {
-                routerConfig.map(item => {
-                    return <NavLink className={styles.link} key={item.path} to={item.path as string}>{item.name}</NavLink>
-                })
+                key: item.path,
+                label: <NavLink key={item.path} to={item.path as string}>{item.name}</NavLink>,
             }
-            <Outlet />
-        </nav>
+        ))
+        setMenus(_menus as MenuItemType[])
+    }, [])
+    return (
+        <Layout>
+            <Header style={{ display: 'flex', alignItems: 'center' }}>
+                <Menu
+                    theme='dark'
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={menus}
+                    style={{ flex: 1, minWidth: 0 }}
+                />
+            </Header>
+            <Content>
+                <Outlet />
+            </Content>
+        </Layout>
+
     )
 }
 
-export default Header
+export default HeaderCustom
