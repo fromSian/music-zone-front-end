d138ca55e9069afc99f70e6864693058df60d577
qian <fromSian@163.com>
2024-04-05 12:23:16 +0800

    feat: vite 代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        rewrite: (path) => {
+          return path.replace(/^\/api/, "");
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, '/src'),
-    }
+      "@": path.resolve(__dirname, "/src"),
+    },
   },
   css: {
     preprocessorOptions: {
@@ -22,4 +33,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
