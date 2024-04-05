2b80cda4a3d957f0f09070c26d44478601791bb1
qian <fromSian@163.com>
2024-04-05 19:36:55 +0800

    feat(banner): image

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,116 +1,211 @@
-import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons'
-import classnames from 'classnames'
-import { MouseEvent, useCallback, useEffect, useRef, useState } from 'react'
-import type { BannerItem } from './data'
-import data from './data'
-import styles from './index.module.less'
+import { AlbumListItem, ListResult } from "@/types/musicInfo";
+import { getErrorMessage } from "@/utils/error";
+import request from "@/utils/request";
+import { LeftCircleTwoTone, RightCircleTwoTone } from "@ant-design/icons";
+import { Empty, Spin } from "antd";
+import { AxiosResponse } from "axios";
+import classnames from "classnames";
+import { map } from "lodash";
+import { MouseEvent, useCallback, useEffect, useRef, useState } from "react";
+import { useQuery } from "react-query";
+import styles from "./index.module.less";
 
-const BannerItem = ({ item, onMouseOver, onMouseLeave }: { item: BannerItem, onMouseOver: (e: MouseEvent) => void, onMouseLeave: (e: MouseEvent) => void, }) => {
-    return <>
-        <div className={styles.banner_item} onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>
-            <p className={styles.banner_item_title}>{item.title} - {item.description}</p>
-            {
-
-            }
+const BannerItem = ({
+  item,
+  onMouseOver,
+  onMouseLeave,
+}: {
+  item: AlbumListItem;
+  onMouseOver: (e: MouseEvent) => void;
+  onMouseLeave: (e: MouseEvent) => void;
+}) => {
+  return (
+    <>
+      <div
+        className={styles.banner_item}
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}
+      >
+        <div className={styles.banner_item_image}>
+          <img src={item.image} className={styles.banner_item_image_main} />
+          <img src={item.image} className={styles.banner_item_image_back} />
         </div>
-    </>
-}
 
-const interval = 3000
-const animationTime = 1000
+        <p className={styles.banner_item_title}>
+          {item.name} - {map(item.artist, "name").join(",")}
+        </p>
+        {}
+      </div>
+    </>
+  );
+};
+
+const interval = 3000;
+const animationTime = 1000;
+
+const page = 1;
+const size = 5;
 
 const Banner = () => {
-    const [bannerData, setBannerData] = useState([data[data.length - 1], ...data, data[0]])
+  const [bannerData, setBannerData] = useState<AlbumListItem[]>([]);
 
-    const [currentIndex, setCurrentIndex] = useState(1)
-    const [translateX, setTranslateX] = useState<number>(-100 / bannerData.length * currentIndex)
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [translateX, setTranslateX] = useState<number>(0);
 
-    const [isAnimate, setIsAnimate] = useState(true)
+  const [isAnimate, setIsAnimate] = useState(true);
 
-    const animateRef = useRef<ReturnType<typeof setInterval>>()
-    const timeoutRef = useRef<ReturnType<typeof setTimeout>>()
+  const animateRef = useRef<ReturnType<typeof setInterval>>();
+  const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
 
-    const move = useCallback((end: number) => {
-        const per = 100 / bannerData.length
-        setTranslateX(-per * end)
-        setCurrentIndex(end)
-    }, [])
+  const { isLoading, isFetching, isSuccess, data } = useQuery({
+    queryKey: ["albums-banner", { page, size }] as const,
+    queryFn: async ({ queryKey }) => {
+      try {
+        const [_key, { page, size }] = queryKey;
+        const result: AxiosResponse<ListResult<AlbumListItem>> =
+          await request.get(`/albums/?page=${page}&size=${size}`);
+        return result.data.results;
+      } catch (err) {
+        console.log(getErrorMessage(err));
+      }
+    },
+  });
 
-
-    const next = useCallback(() => {
-        if (currentIndex === bannerData.length - 2) {
-            setTimeout(() => {
-                setIsAnimate(false)
-                move(1)
-            }, animationTime);
-        } else {
-            setIsAnimate(true)
-        }
-        move(currentIndex + 1)
-    }, [currentIndex])
-
-    useEffect(() => {
-        animateRef.current && clearInterval(animateRef.current)
-        timeoutRef.current && clearTimeout(timeoutRef.current)
-        animateRef.current = undefined
-        timeoutRef.current = undefined
-        // animateRef.current = setInterval(next, interval)
-
-        return () => {
-            animateRef.current && clearInterval(animateRef.current)
-            animateRef.current = undefined
-        }
-    }, [next])
-
-    const onMouseOver = () => {
-        animateRef.current && clearInterval(animateRef.current)
-        timeoutRef.current && clearTimeout(timeoutRef.current)
-        animateRef.current = undefined
-        timeoutRef.current = undefined
+  useEffect(() => {
+    if (isSuccess && data?.length) {
+      const _bannerData = [data[data.length - 1], ...data, data[0]];
+      setBannerData(_bannerData);
+      setCurrentIndex(1);
+      setTranslateX((-100 / _bannerData.length) * 1);
+    } else {
+      setBannerData([]);
     }
+  }, [isSuccess, data]);
 
-    const onMouseLeave = useCallback(() => {
-        timeoutRef.current = setTimeout(() => {
-            animateRef.current = setInterval(next, interval)
-        }, interval)
-    }, [next])
+  const move = useCallback(
+    (end: number) => {
+      const per = 100 / bannerData.length;
+      setTranslateX(-per * end);
+      setCurrentIndex(end);
+      console.log(end);
+    },
+    [bannerData]
+  );
 
+  const next = useCallback(() => {
+    if (currentIndex === bannerData.length - 2) {
+      setTimeout(() => {
+        setIsAnimate(false);
+        move(1);
+      }, animationTime);
+    } else {
+      setIsAnimate(true);
+    }
+    move(currentIndex + 1);
+  }, [currentIndex, bannerData]);
 
-    return (
-        <div className={styles.banner}>
-            <LeftCircleOutlined className={classnames(styles.banner_direction_icon, styles.banner_left_icon,)} onClick={() => {
-                if (currentIndex === 1) {
+  useEffect(() => {
+    animateRef.current && clearInterval(animateRef.current);
+    timeoutRef.current && clearTimeout(timeoutRef.current);
+    animateRef.current = undefined;
+    timeoutRef.current = undefined;
+    // animateRef.current = setInterval(next, interval)
+
+    return () => {
+      animateRef.current && clearInterval(animateRef.current);
+      animateRef.current = undefined;
+    };
+  }, [next]);
+
+  const onMouseOver = () => {
+    animateRef.current && clearInterval(animateRef.current);
+    timeoutRef.current && clearTimeout(timeoutRef.current);
+    animateRef.current = undefined;
+    timeoutRef.current = undefined;
+  };
+
+  const onMouseLeave = useCallback(() => {
+    timeoutRef.current = setTimeout(() => {
+      animateRef.current = setInterval(next, interval);
+    }, interval);
+  }, [next]);
+
+  return (
+    <div className={styles.banner}>
+      {isLoading || isFetching ? (
+        <Spin />
+      ) : (
+        <>
+          {bannerData && bannerData.length ? (
+            <>
+              <LeftCircleTwoTone
+                twoToneColor={[
+                  "rgba(1, 1, 1, 0.6)",
+                  "rgba(255, 255, 255, 0.8)",
+                ]}
+                className={classnames(
+                  styles.banner_direction_icon,
+                  styles.banner_left_icon
+                )}
+                onClick={() => {
+                  if (currentIndex === 1) {
                     setTimeout(() => {
-                        setIsAnimate(false)
-                        move(bannerData.length - 2)
+                      setIsAnimate(false);
+                      move(bannerData.length - 2);
                     }, animationTime);
-                } else {
-                    setIsAnimate(true)
-                }
-                move(currentIndex - 1)
-
-            }} />
-            <RightCircleOutlined className={classnames(styles.banner_direction_icon, styles.banner_right_icon)} onClick={() => {
-                if (currentIndex === bannerData.length - 2) {
+                  } else {
+                    setIsAnimate(true);
+                  }
+                  move(currentIndex - 1);
+                }}
+              />
+              <RightCircleTwoTone
+                twoToneColor={[
+                  "rgba(1, 1, 1, 0.6)",
+                  "rgba(255, 255, 255, 0.8)",
+                ]}
+                className={classnames(
+                  styles.banner_direction_icon,
+                  styles.banner_right_icon
+                )}
+                onClick={() => {
+                  console.log(1);
+                  if (currentIndex === bannerData.length - 2) {
                     setTimeout(() => {
-                        setIsAnimate(false)
-                        move(1)
+                      setIsAnimate(false);
+                      move(1);
                     }, animationTime);
-                } else {
-                    setIsAnimate(true)
-                }
-                move(currentIndex + 1)
-            }} />
-            <div className={classnames(styles.banner_content, { [styles.transition]: isAnimate })} style={{
-                transform: `translateX(${translateX}%)`,
-            }}>
-                {
-                    bannerData.map((item, index) => (<BannerItem onMouseOver={onMouseOver} onMouseLeave={onMouseLeave} key={`banner_item${index}`} item={item} />))
-                }
-            </div>
+                  } else {
+                    setIsAnimate(true);
+                  }
+                  move(currentIndex + 1);
+                }}
+              />
+              <div
+                className={classnames(styles.banner_content, {
+                  [styles.transition]: isAnimate,
+                })}
+                style={{
+                  transform: `translateX(${translateX}%)`,
+                }}
+              >
+                {bannerData.map((item, index) => (
+                  <BannerItem
+                    onMouseOver={onMouseOver}
+                    onMouseLeave={onMouseLeave}
+                    key={`banner_item${index}`}
+                    item={item}
+                  />
+                ))}
+              </div>
+            </>
+          ) : (
+            <Empty />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Banner
+export default Banner;
--- src/types/musicInfo.ts
+++ src/types/musicInfo.ts
@@ -1,3 +1,133 @@
+/**
+ * List 查询分页
+ */
+export interface Links {
+  next: string;
+  previous: any;
+}
+
+/**
+ * List 查询结果
+ */
+export interface ListResult<T> {
+  links: Links;
+  count: number;
+  results: T[];
+}
+
+/**
+ * Artist List 查询结果对象
+ */
+export interface ArtistListItem {
+  id: string;
+  createTime: string;
+  updateTime: string;
+  name: string;
+  image: string;
+  description: string;
+}
+
+/**
+ * Artist Detail 查询结果对象
+ */
+export interface ArtistDetial {
+  id: string;
+  albums: AlbumListItem[];
+  createTime: string;
+  updateTime: string;
+  name: string;
+  image: string;
+  description: string;
+}
+
+/**
+ * Album List 查询结果对象
+ */
+export interface AlbumListItem {
+  id: string;
+  artist: ArtistListItem[];
+  createTime: string;
+  updateTime: string;
+  name: string;
+  image: string;
+  description: string;
+}
+
+/**
+ * Album Detail 查询结果对象
+ */
+export interface AlbumDetail {
+  id: string;
+  artist: ArtistListItem[];
+  songs: Song[];
+  createTime: string;
+  updateTime: string;
+  name: string;
+  image: string;
+  description: string;
+}
+
+/**
+ * Song 查询结果对象
+ */
+export interface Song {
+  id: string;
+  artist: ArtistListItem[];
+  album: AlbumListItem;
+  createTime: string;
+  updateTime: string;
+  name: string;
+  audio: string;
+  track: number;
+  description: string;
+}
+
+/**
+ * playlist List 查询结果对象
+ */
+export interface PlaylistList {
+  id: string;
+  createTime: string;
+  updateTime: string;
+  name: string;
+  image: any;
+  description: string;
+}
+
+/**
+ * playlist add/remove song 传参
+ */
+export interface PlaylistEditSong {
+  song: string;
+}
+
+/**
+ * play record List 查询结果
+ */
+export interface PlayRecordList {
+  id: string;
+  createTime: string;
+  updateTime: string;
+  type: string;
+  targetId: string;
+  count: number;
+  description: string;
+}
+
+/**
+ * play record type
+ */
+export type PlayRecordType = "PLAYLIST" | "ALBUM" | "SONG" | "ARTIST";
+
+/**
+ * play record add one record 传参
+ */
+export interface playRecordEdit {
+  type: PlayRecordType;
+  target_id: string;
+  description: string;
+}
+
 export type BasicSongType = {
   name: string;
   audio: string;
