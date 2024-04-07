5b890027d55f6a9001e1c3e1f644935ba1e28bd1
qian <fromSian@163.com>
2024-04-07 09:59:43 +0800

    fix(排行榜): 列表滚动样式

--- src/components/Ranking/data.ts
+++ src/components/Ranking/data.ts
@@ -1,57 +1,95 @@
 export const rankingTypes = [
-    {
-        label: '总榜',
-        value: 0
-    },
-    {
-        label: '摇滚',
-        value: 1
-    },
-    {
-        label: '民谣',
-        value: 2
-    },
-    {
-        label: '嘻哈',
-        value: 3
-    },
-    {
-        label: '总榜',
-        value: 4
-    },
-    {
-        label: '摇滚',
-        value: 5
-    },
-    {
-        label: '民谣',
-        value: 6
-    },
-    {
-        label: '嘻哈',
-        value: 7
-    },
-]
+  {
+    label: "总榜",
+    value: "all",
+  },
+  {
+    label: "歌曲",
+    value: "SONG",
+  },
+  {
+    label: "专辑",
+    value: "ALBUM",
+  },
+  {
+    label: "歌单",
+    value: "PLAYLIST",
+  },
+  {
+    label: "艺人",
+    value: "ARTIST",
+  },
+];
 
 export const rankingSongs = [
-    {
-        name: '御花园',
-        artist: '苏打绿',
-        id: 0
-    },
-    {
-        name: '人如何学会语言',
-        artist: '郑宜农',
-        id: 1
-    },
-    {
-        name: '我们',
-        artist: '魏如萱',
-        id: 2
-    },
-    {
-        name: '空废',
-        artist: '陈珊妮',
-        id: 3
-    },
-]
+  {
+    name: "御花园",
+    artist: "苏打绿",
+    id: 0,
+  },
+  {
+    name: "人如何学会语言",
+    artist: "郑宜农",
+    id: 1,
+  },
+  {
+    name: "我们",
+    artist: "魏如萱",
+    id: 2,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+  {
+    name: "御花园",
+    artist: "苏打绿",
+    id: 0,
+  },
+  {
+    name: "人如何学会语言",
+    artist: "郑宜农",
+    id: 1,
+  },
+  {
+    name: "我们",
+    artist: "魏如萱",
+    id: 2,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+  {
+    name: "空废",
+    artist: "陈珊妮",
+    id: 3,
+  },
+];
