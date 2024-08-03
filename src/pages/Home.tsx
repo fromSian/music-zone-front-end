0b6c21d11b38187958e9f44751a822dd25119e0f
fromsian <fromSian@163.com>
2024-08-04 06:39:46 +0800

    feat

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
 import Banner from "@/components/Banner/Banner";
 import Recently from "@/components/Recently/Recently";
+import { useEffect, useTransition } from "react";
 import commonStyles from "./common.module.less";
 
 const Home = () => {
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(async () => {
+      console.log("start");
+      setTimeout(() => {
+        console.log("done");
+      }, 2000);
+    });
+  }, []);
   return (
     <div className={commonStyles.content}>
       <Banner />
